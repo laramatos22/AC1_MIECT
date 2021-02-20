cef58d9de598da7b802556c86f879038c47eb4f7
LaraMatos <56411039+laramatos22@users.noreply.github.com>
2021-02-20 18:01:45 +0000

    Update exExtra1.asm

--- AulaPratica4/exExtra1.asm
+++ AulaPratica4/exExtra1.asm
@@ -1,6 +1,6 @@
 # EXERCICIO ADICIONAL 1
 
-# Código em C
+# CÃ³digo em C
 
 #define SIZE 20
 #void main(void)
@@ -12,7 +12,7 @@
 #	p = str;
 #	while (*p != '\0')
 #	{
-#	*p = *p – 'a' + 'A'; 	// 'a'=0x61, 'A'=0x41, 'a'-'A'=0x20
+#	*p = *p â€“ 'a' + 'A'; 	// 'a'=0x61, 'A'=0x41, 'a'-'A'=0x20
 #	p++;
 #	}
 #	print_string(str);
@@ -32,32 +32,32 @@ str1:	.asciiz "Introduza uma string: "
 	.globl main
 	
 main: 	
-	la $a0, str1
-	ori $v0, $0, print_string	# print_string("Introduza um número:");
-	syscall
+	la $a0, str1			# $a0 = str1
+	ori $v0, $0, print_string	# $v0 = print_string
+	syscall				# print_string("Introduza um nÃºmero:");
 	
-	la $a0, str
-	li $a1, SIZE
+	la $a0, str			# $a0 = str -> 1Âº argumento para read_string
+	li $a1, SIZE			# $a1 = SIZE -> 2Âº argumento para read_string
 	li $v0, read_string		# read_string(str, SIZE);
 	syscall
 	
-	la $t0, str
+	la $t0, str			$ p = str;
 	
 while:	
-	lb $t1, 0($t0)			# *p
+	lb $t1, 0($t0)			# *p = str (criaÃ§Ã£o do ponteiro *p)
 	beqz $t1, endWhile		# while (*p != '\0') {
-	blt $t1, 0x61, else		# Verificação da letra de A (0x61)
+	blt $t1, 0x61, else		# VerificaÃ§Ã£o da letra de A (0x61)
 	bgt $t1, 0x7a, else		# a Z (0x7a)
 	
-	sub $t1, $t1, 0x20		# *p = *p – 'a' + 'A'; 	// 'a'=0x61, 'A'=0x41, 'a'-'A'=0x20
-	sb $t1, 0($t0)
+	sub $t1, $t1, 0x20		# *p = *p â€“ 'a' + 'A'; 	// 'a'=0x61, 'A'=0x41, 'a'-'A'=0x20
+	sb $t1, 0($t0)			# renova o registo com os novos caracteres -> store byte
 	
 else: 	
-	addi $t0, $t0, 1		# p++
+	addi $t0, $t0, 1		# 	p++
 	j while				# }
 	
 endWhile:	la $a0, str			
-		ori $v0, $0, print_string	# print_string(str);
+		li $v0, print_string	# print_string(str);
 		syscall
 		
 		jr $ra			# terminar programa
