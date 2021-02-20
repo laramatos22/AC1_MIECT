6edace84b1a6651171ce967352826f4ca57e9610
LaraMatos <56411039+laramatos22@users.noreply.github.com>
2021-02-20 17:06:49 +0000

    Update ex3.asm

--- AulaPratica4/ex3.asm
+++ AulaPratica4/ex3.asm
@@ -1,28 +1,28 @@
 # EXERCICIO 3
 
-# Código em C
+# CÃ³digo em C
 
 #define SIZE 4
 #int array[4] = {7692, 23, 5, 234}; 	// Declara um array global de 4
-#					// posições e inicializa-o
+#					// posiÃ§Ãµes e inicializa-o
 #void main (void)
 #{
 #	int *p; 			// Declara um ponteiro para inteiro
-#					// (não há qualquer inicialização)
+#					// (nÃ£o hÃ¡ qualquer inicializaÃ§Ã£o)
 #	int *pultimo; 			// Declara um ponteiro para inteiro
 #	int soma = 0;
-#	p = array; 			// "p" é preenchido com o endereço
+#	p = array; 			// "p" Ã© preenchido com o endereÃ§o
 #					// inicial do array
-#	pultimo=array+SIZE-1; 		// "pultimo" é inicializado com o
-#					// endereço do último elemento do
+#	pultimo=array+SIZE-1; 		// "pultimo" Ã© inicializado com o
+#					// endereÃ§o do Ãºltimo elemento do
 #					// array, i.e., &array[SIZE-1]
 #	while( p <= pultimo )
 #	{
 #		soma = soma + (*p);
-#		p++; 			// Incrementa o ponteiro (não esquecer
+#		p++; 			// Incrementa o ponteiro (nÃ£o esquecer
 #					// que incrementar um ponteiro para um
 #					// inteiro de 32 bits significa somar a
-#					// quantidade 4 ao valor do endereço)
+#					// quantidade 4 ao valor do endereÃ§o)
 #	}
 #	print_int10(soma);
 #}
@@ -42,22 +42,23 @@ array:	.word 7692, 23, 5, 234
 	
 main:	
 	li $t3, 0			# soma = 0;
-	li $t4, SIZE			#
-	sub $t4, $t4, 1			# $t4 = 3
+	li $t4, SIZE			# $t4 = SIZE
+	sub $t4, $t4, 1			# $t4 = 3 -> SIZE = SIZE - 1
 	sll $t4, $t4, 2			# ou "mul $t4, $t4, 4"		
-					#mul = multiply
+					# mul = multiply
+					
 	la $t0, array			# p = array;
 	addu $t1, $t0, $t4		# pultimo = array + SIZE - 1;	
 
-while:					# while(p <= pultimo)
-	bgtu $t0, $t0, endWhile		# {
-	lw $t2, 0($t0)			# $t2 = *p;
-	add $t3, $t3, $t2		# soma = soma + (*p);
-	addiu $t0, $t0, 4		# p++;
+while:					#
+	bgtu $t0, $t0, endWhile		# while(p <= pultimo) {
+	lw $t2, 0($t0)			# 	$t2 = *p;
+	add $t3, $t3, $t2		# 	soma = soma + (*p);
+	addiu $t0, $t0, 4		# 	p++;
 	j while				# }
 	
 endWhile:	
-	move $a0, $t3
+	move $a0, $t3			# $a0 = soma
 	li $v0, print_int10		# print_int10 (soma);
 	syscall
 	
