75bd8105c64a00a7066021cd4b4320d3954ba585
LaraMatos <56411039+laramatos22@users.noreply.github.com>
2021-02-20 18:18:56 +0000

    Update ex2.asm

--- AulaPratica5/ex2.asm
+++ AulaPratica5/ex2.asm
@@ -1,13 +1,18 @@
 #EXERCICIO 2
 
+# Mapa de registos:
+# $t0 : p
+# $t1 :*p
+# $t2: lista + Size 
+
 	.data
 	
 str1:	.asciiz "\nConteudo do array:\n"
 str2:	.asciiz "; "
-	#.align 2 n„o È necess·rio neste exercicio
+	#.align 2 n√£o √© necess√°rio neste exercicio
 lista:	.word 8,-4,3,5,124,-15,87,9,27,15	#static int lista[]={8, -4, 3, 5, 124, -15, 87, 9, 27, 15};
-						# a diretiva ".word" alinha num endereÁo
-						# m˙ltiplo de 4
+						# a diretiva ".word" alinha num endere√ßo
+						# m√∫ltiplo de 4
 	.eqv SIZE, 10				#define SIZE 10
 	.eqv print_int10, 1
 	.eqv print_string, 4
@@ -15,23 +20,23 @@ lista:	.word 8,-4,3,5,124,-15,87,9,27,15	#static int lista[]={8, -4, 3, 5, 124, 
 	.globl main
 main:						#void main(void)
 						#{
-	la $a0, str1		
-	li $v0, print_string
+	la $a0, str1				# 	$a0 = str1
+	li $v0, print_string			#	$v0 = print_string
 	syscall					#	print_string("\nConteudo do array:\n");
 	
 	la $t0,lista				# 	p = lista
-	li $t2,SIZE				#
-	sll $t2, $t2, 2				#
+	li $t2,SIZE				#	$t2 = SIZE
+	sll $t2, $t2, 2				#	$t2 = $t2 * 4
 	addu $t2,$t0,$t2 			# 	$t2 = lista + SIZE;
 	
 while:	
 	bgeu $t0, $t2, endWhile			# 	while(p < lista + SIZE) {
 	lw $t1, 0($t0)				# 	$t1 = *p;
-						#	endereÁamento indireto do registo
-	or $a0, $0, $t1
+						#	endere√ßamento indireto do registo
+	or $a0, $0, $t1				# 	OU move $a0, $t1
 	li $v0, print_int10		
-	syscall					#	print_int10( *p ); 	// Imprime o conte˙do da posiÁ„o do
-						#				// array cujo endereÁo È "p"
+	syscall					#	print_int10( *p ); 	// Imprime o conte√∫do da posi√ß√£o do
+						#				// array cujo endere√ßo √© "p"
 	
 	la $a0, str2
 	li $v0, print_string	
@@ -43,9 +48,4 @@ while:
 	
 endWhile:
 	jr $ra					# 	termina programa
-
-# Mapa de registos
-# p: $t0
-# *p: $t1
-# lista+Size: $t2
 
